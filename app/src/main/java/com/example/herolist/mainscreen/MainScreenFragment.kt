ce1520dc8a88e87aafc0bfe71789cedafc7cd2ad
Anton <ovsyannikov.a@yahoo.com>
2021-10-04 17:53:12 +0200

    pre set-up
    binding + retrofit init

---
+++ app/src/main/java/com/example/herolist/mainscreen/MainScreenFragment.kt
@@ -0,0 +1,34 @@
+package com.example.herolist.mainscreen
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.herolist.R
+import com.example.herolist.databinding.FragmentMainScreenBinding
+
+class MainScreenFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MainScreenFragment()
+    }
+
+    private lateinit var viewModel: MainScreenViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = FragmentMainScreenBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(MainScreenViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
